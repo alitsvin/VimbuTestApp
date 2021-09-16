806bf284b8a4c17141132c5ae6a1aa46315881a0
Litsvin <alitsvin@i-deasoft.com>
2021-09-16 15:58:58 +0300

    ft: added instructors data to backend

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const data = require('./instructors.json');
+
+app.get('/instructors', (req, res) => {
+  res.send(data);
 });
 
 app.listen(port, () => {
