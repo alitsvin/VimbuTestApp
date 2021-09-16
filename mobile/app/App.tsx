a734461b02525f845525f8c7de583519f09ac7bb
Litsvin <alitsvin@i-deasoft.com>
2021-09-16 16:00:47 +0300

    ft: added new navigation packages

--- mobile/app/App.tsx
+++ mobile/app/App.tsx
@@ -1,33 +1,16 @@
-// import 'react-native-gesture-handler';
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
 /* components */
-import { NavigationContainer } from '@react-navigation/native';
+import {NavigationContainer} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
 import MainRouter from './router/Main';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 export default function App () {
   return (
     <NavigationContainer>
-      {/* <SafeAreaView> */}
-        <MainRouter/>
-      {/* </SafeAreaView> */}
+      <StatusBar barStyle='dark-content'/>
+      <MainRouter/>
     </NavigationContainer>
   );
 };
