7921a2fb51ad1dcededbbbbb22d88f303fd187e9
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 01:01:14 +0300

    ft: added app background color

--- mobile/app/App.tsx
+++ mobile/app/App.tsx
@@ -1,16 +1,27 @@
 import 'react-native-gesture-handler';
 import React from 'react';
+import {StyleSheet} from 'react-native';
 
 /* components */
 import {NavigationContainer} from '@react-navigation/native';
 import {StatusBar} from 'react-native';
 import MainRouter from './router/Main';
+import {View} from 'react-native';
 
 export default function App () {
   return (
     <NavigationContainer>
-      <StatusBar barStyle='dark-content'/>
-      <MainRouter/>
+      <View style={styles.mainContainer}>
+        <StatusBar barStyle='dark-content'/>
+        <MainRouter/>
+      </View>
     </NavigationContainer>
   );
 };
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  }
+});
