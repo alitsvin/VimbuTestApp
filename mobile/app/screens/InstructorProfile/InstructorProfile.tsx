7a41d1e3b2cae28d62c38292f47d305f9cd6d1dc
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 01:49:21 +0300

    ft: background circles for instructor profile

--- mobile/app/screens/InstructorProfile/InstructorProfile.tsx
+++ mobile/app/screens/InstructorProfile/InstructorProfile.tsx
@@ -3,12 +3,19 @@ import {StyleSheet} from 'react-native';
 
 /* components */
 import {View, Text} from 'react-native';
+import BackgroundCircles from './components/BackgroundCircles';
+
+/* ui */
+import {colors, SCREEN_WIDTH} from '@ui';
+
 
 export default function InstructorProfile (): JSX.Element {
 
   return (
     <View style={styles.mainContainer}>
+      {/* header */}
       
+      <BackgroundCircles/>
     </View>
   );
 }
