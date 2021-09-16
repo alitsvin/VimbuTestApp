5a710d50018e186018681ad055b9a6344063f64d
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 01:01:02 +0300

    ft: added colors and todo

--- mobile/app/ui/index.ts
+++ mobile/app/ui/index.ts
@@ -2,3 +2,19 @@ import {Dimensions} from 'react-native';
 
 export const SCREEN_WIDTH = Dimensions.get('screen').width;
 export const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+export const colors = {
+  buttonBlue: '#17C6FA',
+  textDarkGray: '#324149',
+  textGray: '#526169',
+  textLight: '#ffffff',
+  textLightGreen: '#80F2a2',
+};
+
+// TODO:
+// . Add instructors profile
+// . Add services
+// . Add loading to instructors list
+// . add error to instructors list
+// . Add welcome screen
+// . add saving user
