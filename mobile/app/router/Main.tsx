9ee57fef5dd81ed7782781016f475d35e3227e12
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 01:01:31 +0300

    fx: removed header from router

--- mobile/app/router/Main.tsx
+++ mobile/app/router/Main.tsx
@@ -1,23 +1,25 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import { createStackNavigator } from '@react-navigation/stack';
-import {Text} from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 /* screens */
 import InstructorsList from '../screens/InstructorsList/InstructorsList';
 
-const Stack = createNativeStackNavigator();
-// const Stack = createStackNavigator();
+const Stack = createStackNavigator();
 
 export default function MainNavigationStack () {
 
   return (
     <Stack.Navigator
       initialRouteName='InstructorsList'
+      screenOptions={{
+        headerShown: false,
+      }}
     >
-      {/* <Stack.Screen name='InstructorsList' component={Text}/> */}
       {/* <Stack.Screen name='Auth' component={null}/> */}
-      <Stack.Screen name='InstructorsList' component={InstructorsList}/>
+      <Stack.Screen
+        name='InstructorsList'
+        component={InstructorsList}
+      />
       {/* <Stack.Screen name='InstructorProfile' component={null}/> */}
       {/* <Stack.Screen name='Profile' component={null}/> */}
     </Stack.Navigator>
