85b99b8bf83d426eb589fe15400df68aa4d01b7f
Litsvin <alitsvin@i-deasoft.com>
2021-09-16 19:44:31 +0300

    ft: added aliases

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: [
+          './app'
+        ],
+        alias: {
+          '@app': './app',
+          
+          '@components': './app/components',
+          '@screens': './app/screens',
+          '@router': './app/router',
+
+          '@types': './app/types',
+
+          '@services': './app/services',
+          '@utils': './app/utils',
+          '@hooks': './app/hooks',
+
+          '@ui': './app/ui',
+        }
+      } 
+    ] 
+  ]
 };
