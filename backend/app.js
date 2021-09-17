84b80b0374cd99e954a0307b6b0067ccdc99493f
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 04:03:01 +0300

    ft: added fetching instructor by id to backend

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,17 @@ app.get('/instructors', (req, res) => {
   res.send(data);
 });
 
+app.get('/instructors/:instructorId', (req, res, next) => {
+  const id = parseInt(req.params.instructorId);
+  if (typeof id !== 'number') {
+    res.status(400).send(null);
+    return next();
+  }
+
+  const instructor = data.find(i => i.id === id);
+  res.send(instructor);
+});
+
 app.listen(port, () => {
   console.log(`The app listening at http://localhost:${port}`);
 });
