502351bbbc0a0fa34c200d654705505695a45af6
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 04:02:16 +0300

    ft: added icons and status bar height

--- mobile/app/ui/index.ts
+++ mobile/app/ui/index.ts
@@ -1,7 +1,10 @@
 import {Dimensions} from 'react-native';
+import {getStatusBarHeight} from 'react-native-status-bar-height';
 
 export const SCREEN_WIDTH = Dimensions.get('screen').width;
 export const SCREEN_HEIGHT = Dimensions.get('screen').height;
+
+export const STATUS_BAR_HEIGHT = getStatusBarHeight();
 
 export const colors = {
   buttonBlue: '#17C6FA',
