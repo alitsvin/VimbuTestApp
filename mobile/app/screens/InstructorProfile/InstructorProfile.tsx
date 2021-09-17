ffbc19d9f4cbcd6aef6ee7d74fb24918e0e16cbd
Litsvin <alitsvin@i-deasoft.com>
2021-09-17 12:01:09 +0300

    ft: added instructor info

--- mobile/app/screens/InstructorProfile/InstructorProfile.tsx
+++ mobile/app/screens/InstructorProfile/InstructorProfile.tsx
@@ -5,16 +5,20 @@ import {StyleSheet} from 'react-native';
 import {View, Text} from 'react-native';
 import BackgroundCircles from './components/BackgroundCircles';
 import BackHeader from './components/BackHeader';
+import {AvatarImage, BookButton} from '@components';
 
 /* hooks */
+import {useContext} from 'react';
 import {useRoute} from '@react-navigation/native';
 import {useInstructor} from './hooks/useInstructor';
 
 /* ui */
-import {colors, SCREEN_WIDTH} from '@ui';
+import {colors} from '@ui';
 
 /* types */
 import {Instructor} from '@types';
+
+const InstructorContext = React.createContext<Instructor | null>(null);
 
 export default function InstructorProfile (): JSX.Element {
   const {instructorId} = useRoute().params || {};
@@ -22,21 +26,109 @@ export default function InstructorProfile (): JSX.Element {
   
 
   return (
-    <View style={styles.mainContainer}>
-      <BackHeader>Profile</BackHeader>
-      <View style={styles.mainInfoContainer}>
-        {/* header */}
-        {/* bio */}
+    <InstructorContext.Provider value={instructor}>
+      <View style={styles.mainContainer}>
+        <BackHeader>Profile</BackHeader>
+
+        <View style={styles.mainInfoContainer}>
+          <ProfileHeader/>
+          <Description/>
+        </View>
+
+        <View style={styles.bookButtonContainer}>
+          <BookButton/>
+        </View>
+        
+        <BackgroundCircles/>
       </View>
-      
-      <BackgroundCircles/>
+    </InstructorContext.Provider>
+  );
+}
+
+function ProfileHeader (): JSX.Element {
+  const {photo, name, rating} = useContext(InstructorContext) || {};
+  return (
+    <View style={styles.profileHeader}>
+      <AvatarImage
+        src={{uri: photo}}
+        style={styles.photo}
+        size={80}
+      />
+      <View style={styles.profileHeaderRightContainer}>
+        <Text style={styles.name}>{ name }</Text>
+        <Text style={styles.rating}>{ rating }</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  mainContainer: {},
-  mainInfoContainer: {
+function Description (): JSX.Element {
+  const {slogan, bio} = useContext(InstructorContext) || {};
 
+  return (
+    <View style={styles.infoContainer}>
+      <Text style={styles.slogan}>"{ slogan }"</Text>
+      <Text style={styles.bio}>{ bio }</Text>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    paddingBottom: 20,
+  },
+  mainInfoContainer: {
+    padding: 30,
+  },
+  
+  profileHeader: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 0,
+  },
+  photo: {
+    borderColor: colors.textLight,
+  },
+  profileHeaderRightContainer: {
+    marginLeft: 30,
+    marginBottom: 15,
+    alignSelf: 'flex-end',
+  },
+  name: {
+    color: colors.textLight,
+    fontSize: 21,
+    fontWeight: '500',
+    letterSpacing: 1,
+  },
+  rating: {
+    color: colors.textLight,
+    fontSize: 11,
+  },
+
+  infoContainer: {
+    marginTop: 10,
+    flex: 0,
+    maxHeight: 400,
+  },
+  slogan: {
+    color: colors.textLight,
+    fontSize: 18,
+    fontStyle: 'italic',
+    alignSelf: 'center',
+    marginVertical: 20,
+  },
+  bio: {
+    color: colors.textLight,
+    letterSpacing: 0.5,
+    fontSize: 13,
+    lineHeight: 19,
+  },
+
+  bookButtonContainer: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
   },
 });
